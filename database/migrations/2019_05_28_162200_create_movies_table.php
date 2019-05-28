b149cbc8f9b54c769aaeaadaad72d9af3b09b2e1
Vuk Kapor <vukkapor12@gmail.com>
2019-05-28 18:26:50 +0200

    Added movies table

---
+++ database/migrations/2019_05_28_162200_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("title");
+            $table->string("genre");
+            $table->string("director");
+            $table->integer("year");
+            $table->string("storyline");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
