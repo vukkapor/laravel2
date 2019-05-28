34bf4efc29bb6234a07940c45ea9301f07eebdd5
Vuk Kapor <vukkapor12@gmail.com>
2019-05-28 20:03:35 +0200

    Added comments table

---
+++ database/migrations/2019_05_28_175913_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger("movieId");
+            $table->string("content");
+            $table->foreign("movieId")
+                ->references("id")
+                ->on("movies")
+                ->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
